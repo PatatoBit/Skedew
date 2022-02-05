61190c248efca7392af4937cc4adca3c33b8d281
PathonScript <pathonscript@gmail.com>
2022-02-06 06:58:31 +0700

    ipc data and almost done form

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, Menu } = require('electron');
 const path = require('path');
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
@@ -16,15 +16,38 @@ const createWindow = () => {
 
   // and load the index.html of the app.
   mainWindow.loadFile(path.join(__dirname, 'mainWindow.html'));
-
   // Open the DevTools.
   mainWindow.webContents.openDevTools();
+  // Quit app when closed
+  mainWindow.on('closed', function(){
+    app.quit();
+})
 };
+
+const createAddWindow = () => {
+  console.log("Create Add Window");
+  let addWindow = new BrowserWindow({
+    width: 600,
+    height: 450
+  })
+
+  addWindow.loadFile(path.join(__dirname, 'addWindow.html'));
+  addWindow.on('closed', function() {
+    addWindow = null;
+})
+}
+
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
+app.on('ready', function() {
+  createWindow();
+  // Build meny from Template
+  const mainMenu = Menu.buildFromTemplate(mainMenuTemplate)
+  // Insert Menu
+  Menu.setApplicationMenu(mainMenu);
+});
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
@@ -43,5 +66,57 @@ app.on('activate', () => {
   }
 });
 
+
+
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and import them here.
+
+const mainMenuTemplate =[
+  {
+      label: "File",
+      submenu: [{
+          label: "Add item",
+          accelerator: process.platform == 'darwin' ? 'Command+Shift+A' :"Ctrl+Shift+A",
+
+          click(){
+              createAddWindow();
+          }
+      },
+      {
+          label: "Clear Items",
+          click(){
+            console.log("Clear Items")
+          }
+      },
+      {
+          label: "Quit",
+          accelerator: process.platform == 'darwin' ? 'Command+Q' :
+          "Ctrl+Q",
+          click(){
+              app.quit();
+          }
+      }
+  ]
+  }
+];
+
+
+// Add developers tools item if not in production
+if(process.env.NODE_ENV !== "production"){
+  mainMenuTemplate.push({
+      label: "Developer Tools",
+      submenu:[
+          {
+              label: "Toggle DevTools",
+              accelerator: process.platform == 'darwin' ? 'Command+I' :
+              "Ctrl+I",
+              click(item, focusedWindow){
+                  focusedWindow.toggleDevTools();
+              }
+          },
+          {
+              role:"reload"
+          }
+      ]
+  });
+}
